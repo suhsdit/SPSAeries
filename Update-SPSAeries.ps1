28a23c7bb9c300eea6762c44b75ddac62181a306
Jesse Geron <jgeron@suhsd.net>
2024-03-01 12:26:39 -0800

    Updated for current workflow

--- Update-SPSAeries.ps1
+++ Update-SPSAeries.ps1
@@ -10,9 +10,9 @@ Function Update-SPSAeries {
     Write-Host "[X] Removing SPSAeries Module..." -ForegroundColor Green
     Get-Module SPSAeries | Remove-Module
     Write-Host "[X] Importing SPSAeries Module from .\SPSAeries..." -ForegroundColor Green
-    Import-Module .\SPSAeries
+    Import-Module SPSAeries
     Write-Host "[X] Setting SPSAeries config to $config..." -ForegroundColor Green
-    Set-SPSAeriesConfiguration $config
+    Set-SPSAeriesConfiguration $config -verbose
     Write-Host "[X] Updated SPSAeries ready to use. Using Config:" -ForegroundColor Green
     Get-SPSAeriesConfiguration
 }
