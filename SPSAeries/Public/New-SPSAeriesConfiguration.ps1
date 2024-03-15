c2182375d9aba164cbe5750d5e12b87ffcc15d0d
Jesse Geron <jgeron@suhsd.net>
2024-03-15 10:29:10 -0700

    config tests

--- SPSAeries/Public/New-SPSAeriesConfiguration.ps1
+++ SPSAeries/Public/New-SPSAeriesConfiguration.ps1
@@ -1,71 +1,82 @@
 Function New-SPSAeriesConfiguration{
-    <#
-    .SYNOPSIS
-        Setup new configuration to use for the SPSAeries Module
-    .DESCRIPTION
-        Setup new configuration to use for the SPSAeries Module
-    .EXAMPLE
-        New-SPSAeriesConfiguration
-        Start the process of setting config. Follow prompts.
-    .PARAMETER
-    .INPUTS
-    .OUTPUTS
-    .NOTES
-    .LINK
-    #>
-        [CmdletBinding()] #Enable all the default paramters, including -Verbose
-        Param(
-            [Parameter(Mandatory=$false,
-                ValueFromPipeline=$true,
-                ValueFromPipelineByPropertyName=$true,
-                # HelpMessage='HelpMessage',
-                Position=0)]
-            [String]$Name
-        )
-    
-        Begin{
-            Write-Verbose -Message "Starting $($MyInvocation.InvocationName) with $($PsCmdlet.ParameterSetName) parameterset..."
-            Write-Verbose -Message "Parameters are $($PSBoundParameters | Select-Object -Property *)"
-            
-        }
-        Process{
-            # If no users are specified, get all students
-            try{
-                if (!$Name) {
-                    $Name = Read-Host "Config Name"
-                }
+    [CmdletBinding()] 
+    Param(
+        [Parameter(Mandatory=$false,
+            ValueFromPipeline=$true,
+            ValueFromPipelineByPropertyName=$true,
+            Position=0)]
+        [String]$Name,
 
-                if(!(Test-Path -path "$SPSAeriesConfigRoot\$Name")) {
-                    New-Item -ItemType Directory -Name $Name -Path $Script:SPSAeriesConfigRoot
-                    $Script:SPSAeriesConfigDir = "$Script:SPSAeriesConfigRoot\$Name"
+        [Parameter(Mandatory=$false)]
+        [String]$APIURL,
 
-                    Write-Verbose -Message "Setting new Config file"
+        [Parameter(Mandatory=$false)]
+        [PSCredential]$APIKey,
 
+        [Parameter(Mandatory=$false)]
+        [String]$SQLServer,
+
+        [Parameter(Mandatory=$false)]
+        [String]$SQLDB,
+
+        [Parameter(Mandatory=$false)]
+        [PSCredential]$SQLCredentials
+    )
+
+    Begin{
+        Write-Verbose -Message "Starting $($MyInvocation.InvocationName) with $($PsCmdlet.ParameterSetName) parameterset..."
+        Write-Verbose -Message "Parameters are $($PSBoundParameters | Select-Object -Property *)"
+    }
+
+    Process{
+        try{
+            if (!$Name) {
+                $Name = Read-Host "Config Name"
+            }
+
+            if(!(Test-Path -path "$SPSAeriesConfigRoot\$Name")) {
+                New-Item -ItemType Directory -Name $Name -Path $Script:SPSAeriesConfigRoot
+                $Script:SPSAeriesConfigDir = "$Script:SPSAeriesConfigRoot\$Name"
+
+                Write-Verbose -Message "Setting new Config file"
+
+                if (!$APIURL) {
                     $APIURL = Read-Host 'Aeries API URL'
-                    Get-Credential -UserName ' ' -Message 'Enter your Aeries API Key' | Export-Clixml "$SPSAeriesConfigDir\apikey.xml"
+                }
 
+                if (!$APIKey) {
+                    $APIKey = Get-Credential -UserName ' ' -Message 'Enter your Aeries API Key'
+                }
+                $APIKey | Export-Clixml "$SPSAeriesConfigDir\apikey.xml"
+
+                if (!$SQLServer) {
                     $SQLServer = Read-Host 'Aeries SQL DB Server Address'
-                    $SQLDB = Read-Host 'Aeries SQL DB'
-                    Get-credential -Message 'Enter your Aeries SQL credentials' | Export-Clixml "$SPSAeriesConfigDir\sqlcreds.xml"
-                    
-                    @{Config=$Name;APIURL=$APIURL;SQLServer=$SQLServer;SQLDB=$SQLDB} | ConvertTo-Json | Out-File "$SPSAeriesConfigDir\config.json"
+                }
 
-                    # Set the new files as active
-                    Set-SPSAeriesConfiguration $Name
+                if (!$SQLDB) {
+                    $SQLDB = Read-Host 'Aeries SQL DB'
                 }
-                else {
-                    Write-Warning -Message "Config already exists."
-                    break
+
+                if (!$SQLCredentials) {
+                    $SQLCredentials = Get-credential -Message 'Enter your Aeries SQL credentials'
                 }
+                $SQLCredentials | Export-Clixml "$SPSAeriesConfigDir\sqlcreds.xml"
+                
+                @{Config=$Name;APIURL=$APIURL;SQLServer=$SQLServer;SQLDB=$SQLDB} | ConvertTo-Json | Out-File "$SPSAeriesConfigDir\config.json"
+
+                Set-SPSAeriesConfiguration $Name
             }
-            catch{
-                Write-Error -Message "$_ went wrong."
+            else {
+                Write-Warning -Message "Config already exists."
+                break
             }
-            
-            
-            
         }
-        End{
-            Write-Verbose -Message "Ending $($MyInvocation.InvocationName)..."
+        catch{
+            Write-Error -Message "$_ went wrong."
         }
     }
+
+    End{
+        Write-Verbose -Message "Ending $($MyInvocation.InvocationName)..."
+    }
+}
